e4e6f8de51598b5930e5dceb8c89b74b49ebf903
ArtyomKozyrev8 <kozirev8@gmail.com>
2020-11-06 12:09:42 +0300

    added worker_prefetch_multiplier=1 and factory function

--- celery_app/__init__.py
+++ celery_app/__init__.py
@@ -4,12 +4,23 @@ import time
 from settings import R_LOGIN, R_PSWD, R_HOST
 
 broker_url = f"redis://{R_LOGIN}:{R_PSWD}@{R_HOST}:6379"
-celery_app = Celery(
-    'tasks',
-    broker=broker_url,
-    backend=broker_url,
 
-)
+
+def create_celery_app():
+    _celery_app = Celery(
+        'tasks',
+        broker=broker_url,
+        backend=broker_url,
+    )
+
+    _celery_app.conf.update(
+        task_acks_late=True,
+        worker_prefetch_multiplier=1
+    )
+    return _celery_app
+
+
+celery_app = create_celery_app()
 
 
 @celery_app.task()
